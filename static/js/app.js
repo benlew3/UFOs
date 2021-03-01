dc668daaaccbfc330a82bd60ef39611de1ba81ba
Benjamin Lew <bennru33@gmail.com>
2021-02-28 19:46:50 -0800

    update typo

--- static/js/app.js
+++ static/js/app.js
@@ -33,7 +33,7 @@ function updateFilters() {
     let changedElement = d3.select(this);
 
     // 4b. Save the value that was changed as a variable.
-    let elementValue = changeElement.property("value");
+    let elementValue = changedElement.property("value");
 
     // 4c. Save the id of the filter that was changed as a variable.
     let filterId = changedElement.attr("id");
